01a6eced74f4864f557d9d598fb5d08e6897077e
RyzenMu <muruga13in2022@gmail.com>
2023-11-08 04:58:10 +0530

    Regular Expression in Js

---
+++ regex1.js
@@ -0,0 +1,11 @@
+// regex
+// regular expression
+
+let sentence = "The dog chased the cat";
+let regex = /the/;
+
+let myString = "Hello World";
+let myreges = /Hello/;
+
+let result = myreges.test(myString);
+console.log(result);// returns true
